51ab42cee16363538cd03b3d6c7e22aaeac4696c
Lea Ann Bradford <leaann@notch8.com>
2022-08-26 21:49:48 -0700

    rubocop

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,56 +1,60 @@
 # frozen_string_literal: true
 
-# Controller for Recipe actions
-class Api::V1::RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[show update destroy]
+module Api
+  module V1
+    # Controller for Recipe actions
+    class RecipesController < ApplicationController
+      before_action :set_recipe, only: %i[show update destroy]
 
-  # GET /recipes
-  def index
-    @recipes = Recipe.all
+      # GET /recipes
+      def index
+        @recipes = Recipe.all
 
-    render json: @recipes
-  end
+        render json: @recipes
+      end
 
-  # GET /recipes/1
-  def show
-    render json: @recipe
-  end
+      # GET /recipes/1
+      def show
+        render json: @recipe
+      end
 
-  # POST /recipes
-  def create
-    @recipe = Recipe.new(recipe_params)
+      # POST /recipes
+      def create
+        @recipe = Recipe.new(recipe_params)
 
-    if @recipe.save
-      render json: @recipe, status: :created, location: api_v1_recipe_path(@recipe)
-    else
-      render json: @recipe.errors, status: :unprocessable_entity
+        if @recipe.save
+          render json: @recipe, status: :created, location: api_v1_recipe_path(@recipe)
+        else
+          render json: @recipe.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /recipes/1
+      def update
+        if @recipe.update(recipe_params)
+          render json: @recipe
+        else
+          render json: @recipe.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /recipes/1
+      def destroy
+        @recipe.destroy
+      end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_recipe
+        @recipe = Recipe.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def recipe_params
+        params.require(:recipe).permit(:name, :description, :servings, :prep_time, :cook_time, :sprouty_pie,
+                                       :lollys_pantry)
+      end
     end
-  end
-
-  # PATCH/PUT /recipes/1
-  def update
-    if @recipe.update(recipe_params)
-      render json: @recipe
-    else
-      render json: @recipe.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /recipes/1
-  def destroy
-    @recipe.destroy
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_recipe
-    @recipe = Recipe.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def recipe_params
-    params.require(:recipe).permit(:name, :description, :servings, :prep_time, :cook_time, :sprouty_pie,
-                                   :lollys_pantry)
   end
 end
