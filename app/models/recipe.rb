e2b2c4ca71618d3cd4390473ebf9056754669ca1
Lea Ann Bradford <leaann@notch8.com>
2022-11-27 15:55:24 -0800

    add images to api wip

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class Recipe < ApplicationRecord
+  has_one_attached :image
   validates :name, presence: true
+
+  def get_image_url
+    url_for(self.image)
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,4 +65,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << 'lollys-recipe-api'
+  config.active_storage.service = :local
 end
