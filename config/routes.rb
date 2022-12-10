a4058d163dfa490950be0cb1145067a6231e403c
Lea Ann Bradford <leaann@notch8.com>
2022-12-10 12:41:50 -0800

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,12 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
     namespace :v1 do
       resources :recipes
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
       resources :ingredients
     end
   end
